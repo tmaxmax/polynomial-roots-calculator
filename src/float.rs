6143659446f0fe808e6fc44a70d7ca086034fad0
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2023-06-11 16:03:12 +0300

    Create fast floor log for floats

--- src/float.rs
+++ src/float.rs
@@ -1,6 +1,7 @@
 pub trait Float {
     fn near_zero(self) -> bool;
     fn negate(self) -> Self;
+    fn ilog2f(self) -> i32;
 }
 
 pub const TOLERANCE: f64 = 1e-15;
@@ -16,5 +17,14 @@ impl Float for f64 {
         } else {
             -self
         }
+    }
+
+    fn ilog2f(self) -> i32 {
+        debug_assert!(self.is_normal());
+
+        let bits = self.to_bits();
+        let exp = (bits >> 52) & ((1 << 11) - 1);
+
+        exp as i32 - 1023
     }
 }
